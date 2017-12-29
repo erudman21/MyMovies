db4c02f1a1b6185d9a51c9edef58d5cafb3b224f
erudman21 <erudman21@gmail.com>
2017-12-29 10:30:32 -0800

    beginning to setup google oauth flow

---
+++ services/passport.js
@@ -0,0 +1,26 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // Check for existing users, creates a new user if not existing
+      const existingUser = await user.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
