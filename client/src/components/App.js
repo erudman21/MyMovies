76257233ffd5f348d1a6176f04ac30d5a135a9b8
erudman21 <erudman21@gmail.com>
2017-12-30 10:10:56 -0800

    auth reducers and fetching a user

---
+++ client/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+import Header from "./Header";
+import Landing from "./Landing";
+
+//const Header = () => <h2>Header<h2>;
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
