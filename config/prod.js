6ed0b8a34445c2ffc5dec82c1635d1969f844b89
erudman21 <erudman21@gmail.com>
2018-01-10 15:03:05 -0800

    added facebook auth dev keys

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  facebookID: process.env.FB_ID,
+  facebookSecret: process.env.FB_SECRET
 };
