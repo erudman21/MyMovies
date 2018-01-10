fb1ee2dad14a7f02a7530647401eff99f257bc6b
erudman21 <erudman21@gmail.com>
2018-01-10 10:01:52 -0800

    login modal style, got rid of full semantic-ui package

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,21 +1,35 @@
 import React, { Component } from "react";
-import { Button, Modal, Icon } from "semantic-ui-react";
+import { Button, Modal, Icon, Header } from "semantic-ui-react";
 
 class LoginModal extends Component {
   render() {
     const { open, close } = this.props;
     return (
       <Modal size="mini" open={open} onClose={close}>
-        <Modal.Actions>
-          <Button floated="left" href="/auth/facebook" color="facebook">
+        <Header style={{ textAlign: "center" }}>
+          Log in to see your own movies!
+        </Header>
+        <div style={{ width: "90%", marginLeft: "5%" }}>
+          <Button
+            href="/auth/facebook"
+            color="facebook"
+            fluid
+            style={{ marginTop: "5%" }}
+          >
             <Icon name="facebook" />
             Facebook
           </Button>
-          <Button floated="right" href="/auth/google" color="google plus">
+          <br />
+          <Button
+            href="/auth/google"
+            color="google plus"
+            fluid
+            style={{ marginBottom: "5%" }}
+          >
             <Icon name="google plus" />
             Google
           </Button>
-        </Modal.Actions>
+        </div>
       </Modal>
     );
   }
