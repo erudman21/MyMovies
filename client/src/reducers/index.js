264291cbbb8652264162d04635f1b69950bab6f9
erudman21 <erudman21@gmail.com>
2018-01-15 17:32:16 -0800

    got rid of redux form and added back start script

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
-import { reducer as reduxForm } from "redux-form";
 import authReducer from "./authReducer";
 import fetchMoviesReducer from "./fetchMoviesReducer";
 import loadMovieFullReducer from "./loadMovieFullReducer";
@@ -7,7 +6,6 @@ import loadMovieLightReducer from "./loadMovieLightReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm,
   fetchMovies: fetchMoviesReducer,
   loadMovieDataFull: loadMovieFullReducer,
   loadMovieDataLight: loadMovieLightReducer
