1eb05bdc71e6241f2fcd698afd409dba08b444a2
erudman21 <erudman21@gmail.com>
2018-01-22 12:10:32 -0800

    Add message for suggestion list if user doesn't have any movies

--- client/src/components/user/SuggestionList.js
+++ client/src/components/user/SuggestionList.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { List, Card } from 'semantic-ui-react';
+import { List, Card, Header } from 'semantic-ui-react';
 
 const SuggestionList = ({ movies }) => {
 	const renderList = () => {
 		return movies.map(movie => {
 			return movie.recs.map(rec => {
 				return (
-					<List.Item
-						style={{ padding: '0 10px 5px 10px' }}
-						key={rec.name}>
+					<List.Item style={{ padding: '0 10px 5px 10px' }} key={rec.name}>
 						<a href={rec.link}>{rec.name}</a>
 					</List.Item>
 				);
@@ -20,10 +18,22 @@ const SuggestionList = ({ movies }) => {
 		<Card style={{ borderRadius: '0' }}>
 			<Card.Header
 				textAlign="center"
-				style={{ fontSize: '130%', margin: '15px 10px' }}>
+				style={{ fontSize: '130%', margin: '10px 10px' }}>
 				<b>Suggested movies for you</b>
 			</Card.Header>
-			<List style={{ marginTop: '0' }}>{renderList()}</List>
+			{movies.length > 0 ? (
+				<List style={{ marginTop: '0' }}>{renderList()}</List>
+			) : (
+				<Header
+					disabled
+					textAlign="center"
+					style={{
+						fontSize: '100%',
+						margin: '5px auto'
+					}}>
+					Add movies to your list to receive suggestions!
+				</Header>
+			)}
 		</Card>
 	);
 };
