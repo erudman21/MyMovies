c8d66c225897e34e22f0c9a04126e5a921a012b9
Eli Rudman <erudman21@gmail.com>
2018-01-25 14:31:39 -0500

    change spacing

--- client/src/components/movies/MovieList.js
+++ client/src/components/movies/MovieList.js
@@ -1,73 +1,74 @@
-import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
-import MovieCardContent from './MovieCardContent';
+import React, { Component } from "react";
+import { Header } from "semantic-ui-react";
+import MovieCardContent from "./MovieCardContent";
 
 class MovieList extends Component {
-	constructor(props) {
-		super(props);
-		// Loading is initially set to true to allow for fetchmovies to return
-		// and not immediately cause a re-render
-		this.state = { open: false, clicked: null };
-	}
+  constructor(props) {
+    super(props);
+    // Loading is initially set to true to allow for fetchmovies to return
+    // and not immediately cause a re-render
+    this.state = { open: false, clicked: null };
+  }
 
-	openModal = () => this.setState({ open: true });
-	closeModal = () => this.setState({ open: false });
+  openModal = () => this.setState({ open: true });
+  closeModal = () => this.setState({ open: false });
 
-	handleClick = movie => {
-		this.setState({ clicked: movie });
-	};
+  handleClick = movie => {
+    this.setState({ clicked: movie });
+  };
 
-	deleteClicked = () => {
-		const { clicked } = this.state;
+  deleteClicked = () => {
+    const { clicked } = this.state;
 
-		if (clicked != null) {
-			this.props.delClicked(clicked);
-		}
-	};
+    if (clicked != null) {
+      this.props.delClicked(clicked);
+    }
+  };
 
-	// Render a semantic-ui card for each movie in the user's collection
-	renderMovies() {
-		return this.props.movies.map(movie => {
-			return (
-				<MovieCardContent
-					key={movie.title}
-					movie={movie}
-					openModal={this.openModal}
-					closeModal={this.closeModal}
-					open={this.state.open}
-					handleClick={this.handleClick}
-					deleteClicked={this.deleteClicked}
-				/>
-			);
-		});
-	}
+  // Render a semantic-ui card for each movie in the user's collection
+  renderMovies() {
+    return this.props.movies.map(movie => {
+      return (
+        <MovieCardContent
+          key={movie.title}
+          movie={movie}
+          openModal={this.openModal}
+          closeModal={this.closeModal}
+          open={this.state.open}
+          handleClick={this.handleClick}
+          deleteClicked={this.deleteClicked}
+        />
+      );
+    });
+  }
 
-	render() {
-		const { movies: { length } } = this.props;
+  render() {
+    const { movies: { length } } = this.props;
 
-		let content = this.renderMovies();
+    let content = this.renderMovies();
 
-		if (length === 0) {
-			const header = "You don't have any movies!";
-			content = (
-				<Header
-					disabled
-					textAlign="center"
-					style={{
-						fontSize: '250%',
-						marginTop: '30%'
-					}}>
-					{header}
-					<br />
-					<p style={{ fontSize: '20px' }}>
-						Search for movies to add to your journal!
-					</p>
-				</Header>
-			);
-		}
+    if (length === 0) {
+      const header = "You don't have any movies!";
+      content = (
+        <Header
+          disabled
+          textAlign="center"
+          style={{
+            fontSize: "250%",
+            marginTop: "30%"
+          }}
+        >
+          {header}
+          <br />
+          <p style={{ fontSize: "20px" }}>
+            Search for movies to add to your journal!
+          </p>
+        </Header>
+      );
+    }
 
-		return <div>{content}</div>;
-	}
+    return <div>{content}</div>;
+  }
 }
 
 export default MovieList;
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,102 +1,101 @@
-const Path = require('path-parser');
-const requireLogin = require('../middlewares/requireLogin');
-const mongoose = require('mongoose');
-const TDAW = require('tdaw');
-const keys = require('../config/keys');
+const Path = require("path-parser");
+const requireLogin = require("../middlewares/requireLogin");
+const mongoose = require("mongoose");
+const TDAW = require("tdaw");
+const keys = require("../config/keys");
 
-const Movie = mongoose.model('movies');
-const User = mongoose.model('users');
+const Movie = mongoose.model("movies");
+const User = mongoose.model("users");
 
 module.exports = app => {
-	app.post('/api/movies/delete', requireLogin, async (req, res) => {
-		try {
-			Movie.findOneAndRemove({
-				_user: req.user.id,
-				title: req.body.movie.title
-			}).exec();
+  app.post("/api/movies/delete", requireLogin, async (req, res) => {
+    try {
+      Movie.findOneAndRemove({
+        _user: req.user.id,
+        title: req.body.movie.title
+      }).exec();
 
-			User.findById(req.user._id, (e, user) => {
-				user.avgRating =
-					user.numMovies <= 1
-						? 0
-						: (user.avgRating * user.numMovies -
-								req.body.movie.personalRating) /
-							(user.numMovies - 1);
-				user.numMovies--;
-				user.save((e, updatedUser) => {
-					res.send(updatedUser);
-				});
-			});
-		} catch (err) {
-			res.status(422).send(err);
-		}
-	});
+      User.findById(req.user._id, (e, user) => {
+        user.avgRating =
+          user.numMovies <= 1
+            ? 0
+            : (user.avgRating * user.numMovies -
+                req.body.movie.personalRating) /
+              (user.numMovies - 1);
+        user.numMovies--;
+        user.save((e, updatedUser) => {
+          res.send(updatedUser);
+        });
+      });
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
 
-	app.get('/api/movies', requireLogin, async (req, res) => {
-		const movies = await Movie.find({ _user: req.user.id });
-		// Reverse movies so newly added movies appear first
-		movies.reverse();
-		res.send(movies);
-	});
+  app.get("/api/movies", requireLogin, async (req, res) => {
+    const movies = await Movie.find({ _user: req.user.id });
+    // Reverse movies so newly added movies appear first
+    movies.reverse();
+    res.send(movies);
+  });
 
-	app.post('/api/movies', requireLogin, async (req, res) => {
-		const {
-			title,
-			image,
-			officialRatings,
-			year,
-			director,
-			dateSeen,
-			personalRating,
-			personalComments
-		} = req.body;
+  app.post("/api/movies", requireLogin, async (req, res) => {
+    const {
+      title,
+      image,
+      officialRatings,
+      year,
+      director,
+      dateSeen,
+      personalRating,
+      personalComments
+    } = req.body;
 
-		const tdaw = new TDAW({ apiKey: keys.tdKey });
-		let recs = [];
+    const tdaw = new TDAW({ apiKey: keys.tdKey });
+    let recs = [];
 
-		try {
-			const temp = await tdaw.getRecommendations({
-				q: title,
-				type: 'movies',
-				verbose: '1'
-			}); 
+    try {
+      const temp = await tdaw.getRecommendations({
+        q: title,
+        type: "movies",
+        verbose: "1"
+      });
 
-			for (var i = 0; i < 5; i++) {
-				recs[i] = {
-					name: temp[i].Name,
-					link: temp[i].yUrl
-				};
-			}
-		} catch (e) {}
+      for (var i = 0; i < 5; i++) {
+        recs[i] = {
+          name: temp[i].Name,
+          link: temp[i].yUrl
+        };
+      }
+    } catch (e) {}
 
-		// Create a new movie object with all of the appropriate fields
-		const movie = new Movie({
-			title,
-			image,
-			officialRatings,
-			year,
-			director,
-			recs,
-			dateSeen,
-			personalRating,
-			personalComments,
-			_user: req.user.id
-		});
+    // Create a new movie object with all of the appropriate fields
+    const movie = new Movie({
+      title,
+      image,
+      officialRatings,
+      year,
+      director,
+      recs,
+      dateSeen,
+      personalRating,
+      personalComments,
+      _user: req.user.id
+    });
 
-		try {
-			// Save the movie to the user model
-			await movie.save();
+    try {
+      // Save the movie to the user model
+      await movie.save();
 
-			User.findById(req.user._id, (e, user) => {
-				user.numMovies++;
-				user.avgRating +=
-					(personalRating - user.avgRating) / user.numMovies;
-				user.save((e, updatedUser) => {
-					res.send(updatedUser);
-				});
-			});
-		} catch (err) {
-			res.status(422).send(err);
-		}
-	});
+      User.findById(req.user._id, (e, user) => {
+        user.numMovies++;
+        user.avgRating += (personalRating - user.avgRating) / user.numMovies;
+        user.save((e, updatedUser) => {
+          res.send(updatedUser);
+        });
+      });
+    } catch (err) {
+      res.status(422).send(err);
+    }
+  });
 };
