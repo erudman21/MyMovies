93ee4fe81a5fe2ca40a80663510f8000e70142b6
Eli Rudman <erudman21@gmail.com>
2018-01-25 14:39:54 -0500

    Fixed profile image display

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,110 +1,112 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import MovieList from './movies/MovieList';
-import * as actions from '../actions';
-import SiteHeader from './SiteHeader';
-import { Loader, Container } from 'semantic-ui-react';
-import SuggestionList from './user/SuggestionList';
-import ProfileDisplay from './user/ProfileDisplay';
-import MoviesNear from './user/MoviesNear';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import MovieList from "./movies/MovieList";
+import * as actions from "../actions";
+import SiteHeader from "./SiteHeader";
+import { Loader, Container } from "semantic-ui-react";
+import SuggestionList from "./user/SuggestionList";
+import ProfileDisplay from "./user/ProfileDisplay";
+import MoviesNear from "./user/MoviesNear";
 
 class Dashboard extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			loading: true,
-			location: { lat: '', long: '' }
-		};
-	}
+    this.state = {
+      loading: true,
+      location: { lat: "", long: "" }
+    };
+  }
 
-	getLocation = () => {
-		if ('geolocation' in navigator) {
-			navigator.geolocation.getCurrentPosition(position => {
-				this.setState({
-					location: {
-						lat: position.coords.latitude,
-						long: position.coords.longitude
-					}
-				});
-			});
-		}
-	};
+  getLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({
+          location: {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          }
+        });
+      });
+    }
+  };
 
-	// Delete whatever clicked is set to in state
-	delClicked = movie => {
-		const { deleteMovie, fetchMovies } = this.props;
+  // Delete whatever clicked is set to in state
+  delClicked = movie => {
+    const { deleteMovie, fetchMovies } = this.props;
 
-		this.setState({ loading: true });
-		deleteMovie({ movie }).then(() => {
-			fetchMovies().then(() => {
-				this.setState({ open: false, loading: false });
-			});
-		});
-	};
+    this.setState({ loading: true });
+    deleteMovie({ movie }).then(() => {
+      fetchMovies().then(() => {
+        this.setState({ open: false, loading: false });
+      });
+    });
+  };
 
-	componentDidMount = () => {
-		this.props.fetchUser();
-		this.props.fetchMovies().then(() => {
-			this.setState({ loading: false });
-		});
-		this.getLocation();
-	};
+  componentDidMount = () => {
+    this.props.fetchUser();
+    this.props.fetchMovies().then(() => {
+      this.setState({ loading: false });
+    });
+    this.getLocation();
+  };
 
-	render() {
-		const { movies, auth } = this.props;
-		const { loading, location } = this.state;
+  render() {
+    const { movies, auth } = this.props;
+    const { loading, location } = this.state;
 
-		return (
-			<div>
-				<SiteHeader />
-				<div
-					style={{
-						backgroundColor: '#f7f7f4',
-						position: 'absolute',
-						margin: '0 auto',
-						width: '100%',
-						minHeight: '100%'
-					}}>
-					{loading ? (
-						<Loader active inline="centered" style={{ marginTop: '20%' }} />
-					) : (
-						<Container
-							style={{
-								marginTop: '15px',
-								display: 'grid',
-								gridTemplateColumns:
-									'195px minmax(500px, 1fr) 195px',
-								gridTemplateRows: 'minmax(117px, 120px) auto',
-								gridGap: '15px'
-							}}>
-							<div
-								style={{
-									float: 'right',
-									gridColumn: '1',
-									gridRow: '1'
-								}}>
-								<ProfileDisplay user={auth} />
-							</div>
-							<div style={{ float: 'right', gridColumn: '1', gridRow: '2' }}>
-								<SuggestionList movies={movies} />
-							</div>
-							<div style={{ gridColumn: '2' }}>
-								<MovieList movies={movies} delClicked={this.delClicked} />
-							</div>
-							<div style={{ gridColumn: '3' }}>
-								<MoviesNear location={location} />
-							</div>
-						</Container>
-					)}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <SiteHeader />
+        <div
+          style={{
+            backgroundColor: "#f7f7f4",
+            position: "absolute",
+            margin: "0 auto",
+            width: "100%",
+            minHeight: "100%"
+          }}
+        >
+          {loading ? (
+            <Loader active inline="centered" style={{ marginTop: "20%" }} />
+          ) : (
+            <Container
+              style={{
+                marginTop: "15px",
+                display: "grid",
+                gridTemplateColumns: "195px minmax(500px, 1fr) 195px",
+                gridTemplateRows: "minmax(117px, 120px) auto",
+                gridGap: "15px"
+              }}
+            >
+              <div
+                style={{
+                  float: "right",
+                  gridColumn: "1",
+                  gridRow: "1"
+                }}
+              >
+                <ProfileDisplay user={auth} />
+              </div>
+              <div style={{ float: "right", gridColumn: "1", gridRow: "2" }}>
+                <SuggestionList movies={movies} />
+              </div>
+              <div style={{ gridColumn: "2" }}>
+                <MovieList movies={movies} delClicked={this.delClicked} />
+              </div>
+              <div style={{ gridColumn: "3" }}>
+                <MoviesNear location={location} />
+              </div>
+            </Container>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps({ fetchMovies, auth }) {
-	return { movies: fetchMovies, auth };
+  return { movies: fetchMovies, auth };
 }
 
 export default connect(mapStateToProps, actions)(Dashboard);
--- client/src/components/user/ProfileDisplay.js
+++ client/src/components/user/ProfileDisplay.js
@@ -1,44 +1,45 @@
-import React from 'react';
-import { Card, List, Header } from 'semantic-ui-react';
+import React from "react";
+import { Card, List, Header } from "semantic-ui-react";
 
 const ProfileDisplay = ({ user }) => {
-	return (
-		<Card style={{ borderRadius: '0' }}>
-			<div style={{ overflow: 'hidden' }}>
-				<img
-					src={require('../../media/film-roll.png')}
-					style={{ width: '110%', marginLeft: '-5%' }}
-					alt="film-roll"
-				/>
-			</div>
-			<Header
-				textAlign="center"
-				style={{
-					fontSize: '130%',
-					margin: '0 auto'
-				}}>
-				<b>{user.name}</b>
-			</Header>
-			<Card.Meta textAlign="center">
-				<List horizontal>
-					<List.Item>
-						<List.Content style={{ textAlign: 'center' }}>
-							<b>Movies</b>
-							<br />
-							{user.numMovies ? user.numMovies : 0}
-						</List.Content>
-					</List.Item>
-					<List.Item>
-						<List.Content style={{ textAlign: 'center' }}>
-							<b>Average Rating</b>
-							<br />
-							{user.avgRating ? user.avgRating.toFixed(2) : 0}
-						</List.Content>
-					</List.Item>
-				</List>
-			</Card.Meta>
-		</Card>
-	);
+  return (
+    <Card style={{ borderRadius: "0", overflow: "hidden" }}>
+      <div>
+        <img
+          src={require("../../media/film-roll.png")}
+          style={{ width: "110%", marginLeft: "-4%" }}
+          alt="film-roll"
+        />
+      </div>
+      <Header
+        textAlign="center"
+        style={{
+          fontSize: "130%",
+          margin: "0 auto"
+        }}
+      >
+        <b>{user.name}</b>
+      </Header>
+      <Card.Meta textAlign="center">
+        <List horizontal>
+          <List.Item>
+            <List.Content style={{ textAlign: "center" }}>
+              <b>Movies</b>
+              <br />
+              {user.numMovies ? user.numMovies : 0}
+            </List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Content style={{ textAlign: "center" }}>
+              <b>Average Rating</b>
+              <br />
+              {user.avgRating ? user.avgRating.toFixed(2) : 0}
+            </List.Content>
+          </List.Item>
+        </List>
+      </Card.Meta>
+    </Card>
+  );
 };
 
 export default ProfileDisplay;
