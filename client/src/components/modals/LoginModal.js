73ed76a539817a560a97c39055ad3acb5aeb64d0
Eli Rudman <erudman21@gmail.com>
2018-02-01 17:30:14 -0500

    disabled facebook button because of internal error

--- client/src/components/modals/LoginModal.js
+++ client/src/components/modals/LoginModal.js
@@ -2,60 +2,64 @@ import React, { Component } from 'react';
 import { Button, Modal, Icon, Header, Dimmer, Loader } from 'semantic-ui-react';
 
 class LoginModal extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			loading: false
-		};
-	}
+    this.state = {
+      loading: false
+    };
+  }
 
-	startLoading() {
-		this.setState({ loading: true });
+  startLoading() {
+    this.setState({ loading: true });
 
-		setTimeout(() => this.setState({ loading: false }), 4000);
-	}
+    setTimeout(() => this.setState({ loading: false }), 4000);
+  }
 
-	render() {
-		const { open, close } = this.props;
-		const { loading } = this.state;
+  render() {
+    const { open, close } = this.props;
+    const { loading } = this.state;
 
-		return (
-			<Modal
-				size="mini"
-				open={open}
-				onClose={close}
-				style={{ borderRadius: '5px' }}>
-				{loading ? (
-					<Dimmer active>
-						<Loader />
-					</Dimmer>
-				) : null}
-				<Header style={{ textAlign: 'center', borderRadius: '5px' }}>
-					Log in to see your own movies!
-				</Header>
-				<div style={{ width: '90%', marginLeft: '5%' }}>
-					<Button
-						onClick={() => this.startLoading()}
-						href="/auth/facebook"
-						color="facebook"
-						fluid
-						style={{ marginTop: '5%' }}>
-						<Icon name="facebook" /> Facebook
-					</Button>
-					<br />
-					<Button
-						onClick={() => this.startLoading()}
-						href="/auth/google"
-						color="google plus"
-						fluid
-						style={{ marginBottom: '5%' }}>
-						<Icon name="google plus" /> Google
-					</Button>
-				</div>
-			</Modal>
-		);
-	}
+    return (
+      <Modal
+        size="mini"
+        open={open}
+        onClose={close}
+        style={{ borderRadius: '5px' }}
+      >
+        {loading ? (
+          <Dimmer active>
+            <Loader />
+          </Dimmer>
+        ) : null}
+        <Header style={{ textAlign: 'center', borderRadius: '5px' }}>
+          Log in to see your own movies!
+        </Header>
+        <div style={{ width: '90%', marginLeft: '5%' }}>
+          <Button
+            disabled
+            onClick={() => this.startLoading()}
+            href="/auth/facebook"
+            color="facebook"
+            fluid
+            style={{ marginTop: '5%' }}
+          >
+            <Icon name="facebook" /> Facebook - Sorry, not currently available
+          </Button>
+          <br />
+          <Button
+            onClick={() => this.startLoading()}
+            href="/auth/google"
+            color="google plus"
+            fluid
+            style={{ marginBottom: '5%' }}
+          >
+            <Icon name="google plus" /> Google
+          </Button>
+        </div>
+      </Modal>
+    );
+  }
 }
 
 export default LoginModal;
