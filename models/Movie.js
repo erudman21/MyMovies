fd1e66a624837555d077bd1789d2448a58e0e632
Eli Rudman <erudman21@gmail.com>
2018-02-06 20:17:12 -0500

    add localusername/password to user model

--- models/Movie.js
+++ models/Movie.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const movieSchema = new Schema({
-	title: String,
-	image: String,
-	officialRatings: [{ Source: String, Value: String }],
-	year: String,
-	director: String,
-	recs: [{ name: String, link: String }],
-	dateSeen: Date,
-	personalRating: Number,
-	personalComments: String,
-	_user: { type: Schema.Types.ObjectId, ref: 'User' }
+  title: String,
+  image: String,
+  officialRatings: [{ Source: String, Value: String }],
+  year: String,
+  director: String,
+  recs: [{ name: String, link: String }],
+  dateSeen: Date,
+  personalRating: Number,
+  personalComments: String,
+  _user: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 mongoose.model('movies', movieSchema);
--- models/User.js
+++ models/User.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-	googleId: String,
-	facebookId: String,
-	name: String,
-	numMovies: { type: Number, default: 0 },
-	avgRating: { type: Number, default: 0 }
+  localUsername: String,
+  localPassword: String,
+  googleId: String,
+  facebookId: String,
+  name: String,
+  numMovies: { type: Number, default: 0 },
+  avgRating: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
