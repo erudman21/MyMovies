7c9374fb5ef163b2573370d08c026c4713a6f9f6
Eli Rudman <erudman21@gmail.com>
2018-02-10 15:27:08 -0500

    add bcrypt hashing for local passwords

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -10,5 +11,30 @@ const userSchema = new Schema({
   numMovies: { type: Number, default: 0 },
   avgRating: { type: Number, default: 0 }
 });
+
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.localPassword) {
+    if (!user.isModified('localPassword')) return next();
+
+    bcrypt.genSalt(5, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.localPassword, salt, null, (err, hash) => {
+        if (err) return next(err);
+        user.localPassword = hash;
+        next();
+      });
+    });
+  }
+});
+
+userSchema.methods.verifyPassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 mongoose.model('users', userSchema);
