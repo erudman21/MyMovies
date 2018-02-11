7407244dbd866ad96bcceac418f586ecf975d53d
Eli Rudman <erudman21@gmail.com>
2018-02-11 00:29:04 -0500

    add form action for local register

--- client/src/components/modals/RegisterModal.js
+++ client/src/components/modals/RegisterModal.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import { Modal, Button, Form } from 'semantic-ui-react';
 
 class RegisterModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleInput = (e, { value, name }) => this.setState({ [name]: value });
+
   render() {
     const { open, close } = this.props;
+    const { username, password } = this.state;
 
     return (
       <Modal open={open} onClose={close} size="mini">
@@ -11,37 +23,40 @@ class RegisterModal extends Component {
           Create a new account
         </Modal.Header>
         <Modal.Content>
-          <Form size="small">
+          <Form size="small" action="/auth/local/register" method="POST">
             <Form.Input
+              value={username}
+              name="username"
+              onChange={this.handleInput}
               fluid
               icon="user"
               iconPosition="left"
               placeholder="Username"
             />
             <Form.Input
+              value={password}
+              name="password"
+              onChange={this.handleInput}
               fluid
               icon="lock"
               iconPosition="left"
               placeholder="Password"
-              type="password"
+            />
+            <Button
+              color="red"
+              icon="arrow left"
+              content="Back"
+              onClick={close}
+            />
+            <Button
+              icon="check"
+              content="Register"
+              type="submit"
+              color="instagram"
+              floated="right"
             />
           </Form>
         </Modal.Content>
-        <Modal.Actions>
-          <Button
-            color="red"
-            icon="arrow left"
-            content="Back"
-            onClick={close}
-            floated="left"
-          />
-          <Button
-            icon="check"
-            content="Register"
-            href="/movies"
-            color="instagram"
-          />
-        </Modal.Actions>
       </Modal>
     );
   }
