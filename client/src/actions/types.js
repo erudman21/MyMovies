e9b27614637f440a042cfe76d5b662688fac30dc
Eli Rudman <erudman21@gmail.com>
2018-02-13 19:31:13 -0500

    add flash message type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -3,3 +3,4 @@ export const FETCH_USER_MOVIES = 'fetch_user_movies';
 export const LOAD_MOVIE_DATA_FULL = 'load_movie_data_full';
 export const LOAD_MOVIE_DATA_LIGHT = 'load_movie_data_light';
 export const FETCH_FANDANGO_MOVIES = 'fetch_fandango_movies';
+export const LOCAL_AUTH_FLASH = 'local_auth_flash';
